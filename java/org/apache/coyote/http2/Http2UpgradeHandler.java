276c8f0b02bdbd03f9221011ad7a0e8d723f239c
Amit Agam <amit.a@sealsecurity.io>
2024-05-12 13:39:09 +0300

    CVE-2023-44487

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -33,6 +33,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 import javax.servlet.http.WebConnection;
 
@@ -356,7 +357,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                                 stream.close(se);
                             }
                         } finally {
-                            if (overheadCount.get() > 0) {
+                            if (isOverheadLimitExceeded()) {
                                 throw new ConnectionException(
                                         sm.getString("upgradeHandler.tooMuchOverhead", connectionId),
                                         Http2Error.ENHANCE_YOUR_CALM);
@@ -1463,6 +1464,11 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
+    boolean isOverheadLimitExceeded() {
+        return overheadCount.get() > 0;
+    }
+
+
     // ----------------------------------------------- Http2Parser.Input methods
 
     @Override
@@ -1723,6 +1729,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
             log.debug(sm.getString("upgradeHandler.reset.receive", getConnectionId(), Integer.toString(streamId),
                     Long.toString(errorCode)));
         }
+        increaseOverheadCount(FrameType.RST, getProtocol().getOverheadResetFactor());
         AbstractNonZeroStream abstractNonZeroStream = getAbstractNonZeroStream(streamId, true);
         abstractNonZeroStream.checkState(FrameType.RST);
         if (abstractNonZeroStream instanceof Stream) {

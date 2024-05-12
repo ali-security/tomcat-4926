5eef7c9c822b6950fc1d5504bc891d30a69ce27b
Amit Agam <amit.a@sealsecurity.io>
2024-05-12 14:41:55 +0300

    Revert "CVE-2023-44487"
    This reverts commit 276c8f0b02bdbd03f9221011ad7a0e8d723f239c.

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -356,7 +356,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                                 stream.close(se);
                             }
                         } finally {
-                            if (isOverheadLimitExceeded()) {
+                            if (overheadCount.get() > 0) {
                                 throw new ConnectionException(
                                         sm.getString("upgradeHandler.tooMuchOverhead", connectionId),
                                         Http2Error.ENHANCE_YOUR_CALM);
@@ -1463,11 +1463,6 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
-    boolean isOverheadLimitExceeded() {
-        return overheadCount.get() > 0;
-    }
-
-
     // ----------------------------------------------- Http2Parser.Input methods
 
     @Override
@@ -1728,7 +1723,6 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
             log.debug(sm.getString("upgradeHandler.reset.receive", getConnectionId(), Integer.toString(streamId),
                     Long.toString(errorCode)));
         }
-        increaseOverheadCount(FrameType.RST, getProtocol().getOverheadResetFactor());
         AbstractNonZeroStream abstractNonZeroStream = getAbstractNonZeroStream(streamId, true);
         abstractNonZeroStream.checkState(FrameType.RST);
         if (abstractNonZeroStream instanceof Stream) {
